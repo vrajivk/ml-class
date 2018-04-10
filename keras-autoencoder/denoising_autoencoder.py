747f2fc20371e78cf47fd1a8b7e57666fd7c168e
Rajiv Vijayakumar <rvijayak@qti.qualcomm.com>
2018-04-10 14:33:06 -0700

    run black

--- keras-autoencoder/denoising_autoencoder.py
+++ keras-autoencoder/denoising_autoencoder.py
@@ -6,14 +6,20 @@ import numpy as np
 import wandb
 from wandb.wandb_keras import WandbKerasCallback
 
+
 def add_noise(x_train, x_test):
     noise_factor = 0.5
-    x_train_noisy = x_train + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_train.shape) 
-    x_test_noisy = x_test + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_test.shape) 
-    
+    x_train_noisy = x_train + noise_factor * np.random.normal(
+        loc=0.0, scale=1.0, size=x_train.shape
+    )
+    x_test_noisy = x_test + noise_factor * np.random.normal(
+        loc=0.0, scale=1.0, size=x_test.shape
+    )
+
     x_train_noisy = np.clip(x_train_noisy, 0., 1.)
     x_test_noisy = np.clip(x_test_noisy, 0., 1.)
     return x_train_noisy, x_test_noisy
+
 
 run = wandb.init()
 config = run.config
@@ -25,36 +31,41 @@ config.epochs = 10
 (x_train_noisy, x_test_noisy) = add_noise(x_train, x_test)
 
 
-x_train = x_train.astype('float32') / 255.
-x_test = x_test.astype('float32') / 255.
+x_train = x_train.astype("float32") / 255.
+x_test = x_test.astype("float32") / 255.
 
 
 model = Sequential()
-model.add(Flatten(input_shape=(28,28)))
-model.add(Dense(config.encoding_dim, activation='relu'))
-model.add(Dense(784, activation='sigmoid'))
-model.add(Reshape((28,28)))
-model.compile(optimizer='adam', loss='mse')
+model.add(Flatten(input_shape=(28, 28)))
+model.add(Dense(config.encoding_dim, activation="relu"))
+model.add(Dense(784, activation="sigmoid"))
+model.add(Reshape((28, 28)))
+model.compile(optimizer="adam", loss="mse")
+
 
 class Images(Callback):
-      def on_epoch_end(self, epoch, logs):
-            indices = np.random.randint(self.validation_data[0].shape[0], size=8)
-            test_data = self.validation_data[0][indices]
-            pred_data = self.model.predict(test_data)
-            run.history.row.update({
-                  "examples": [
-                        wandb.Image(np.hstack([data, pred_data[i]]), caption=str(i))
-                        for i, data in enumerate(test_data)]
-            })
+
+    def on_epoch_end(self, epoch, logs):
+        indices = np.random.randint(self.validation_data[0].shape[0], size=8)
+        test_data = self.validation_data[0][indices]
+        pred_data = self.model.predict(test_data)
+        run.history.row.update(
+            {
+                "examples": [
+                    wandb.Image(np.hstack([data, pred_data[i]]), caption=str(i))
+                    for i, data in enumerate(test_data)
+                ]
+            }
+        )
 
 
-model.fit(x_train_noisy, x_train,
-                epochs=config.epochs,
-                validation_data=(x_test_noisy, x_test), callbacks=[Images(), WandbKerasCallback()])
+model.fit(
+    x_train_noisy,
+    x_train,
+    epochs=config.epochs,
+    validation_data=(x_test_noisy, x_test),
+    callbacks=[Images(), WandbKerasCallback()],
+)
 
 
 model.save("auto-denoise.h5")
-
-
-
-
