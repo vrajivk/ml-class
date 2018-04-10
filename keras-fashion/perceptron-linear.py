bf63fc8db59e948bd02567edaa6ef0b61e11876e
Rajiv Kumar <rajiv.kumar+githubpush@gmail.com>
2018-04-10 20:47:18 +0000

    Final fashion

--- keras-fashion/perceptron-linear.py
+++ keras-fashion/perceptron-linear.py
@@ -1,18 +1,22 @@
 import numpy
 from keras.datasets import fashion_mnist
 from keras.models import Sequential
-from keras.layers import Dense, Flatten, Dropout
+from keras.layers import Dense, Flatten, Dropout, Reshape, Conv2D, MaxPooling2D 
 from keras.utils import np_utils
+from keras.callbacks import EarlyStopping
 import wandb
 from wandb.wandb_keras import WandbKerasCallback
 
 # logging code
 run = wandb.init()
 config = run.config
-config.epochs = 10
+config.epochs = 100
 
 # load data
 (X_train, y_train), (X_test, y_test) = fashion_mnist.load_data()
+
+X_train = X_train/255
+X_test = X_test/255
 
 img_width = X_train.shape[1]
 img_height = X_train.shape[2]
@@ -27,14 +31,25 @@ num_classes = y_train.shape[1]
 
 # create model
 model=Sequential()
-model.add(Flatten(input_shape=(img_width,img_height)))
-model.add(Dense(num_classes))
-model.compile(loss='mse', optimizer='adam',
+model.add(Reshape((img_width, img_height, 1), input_shape=(img_width,img_height)))
+#model.add(Dropout(0.4))
+model.add(Conv2D(32, (3,3), activation='relu'))
+model.add(MaxPooling2D(2,2))
+model.add(Dropout(0.3))
+model.add(Conv2D(64, (3,3), activation='relu'))
+#model.add(MaxPooling2D(2,2))
+model.add(Dropout(0.3))
+model.add(Flatten())
+model.add(Dense(100, activation='relu'))
+model.add(Dropout(0.3))
+model.add(Dense(num_classes, activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam',
                 metrics=['accuracy'])
 
+print(model.summary())
 # Fit the model
 model.fit(X_train, y_train, epochs=config.epochs, validation_data=(X_test, y_test),
-                    callbacks=[WandbKerasCallback(validation_data=X_test, labels=labels)])
+                    callbacks=[WandbKerasCallback(validation_data=X_test, labels=labels), EarlyStopping(monitor='val_acc', min_delta=0.001,patience=3, verbose=1)])
 
 
 
